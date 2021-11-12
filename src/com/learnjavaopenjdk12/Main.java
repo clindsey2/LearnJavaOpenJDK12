60ab39c10df84722c2468e82fbabc7d0ff50d100
clindsey2 <clindsey2@liberty.edu>
2021-11-12 11:08:19 -0500

    reference types

--- src/com/learnjavaopenjdk12/Main.java
+++ src/com/learnjavaopenjdk12/Main.java
@@ -1,13 +1,22 @@
 package com.learnjavaopenjdk12;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
+        /* Primitive types */
         byte myAge = 30;
         long viewsCount = 3_123_456_789L;    /*underscores make the number more readable & L indicates long*/
         float price = 10.99F;   /*the F tells java to store this as a float*/
         char letter = 'A';  /* wrap single character with single quote and multiple characters with double quotes*/
         boolean isEligible = false;   /*words in orange are reserved keywords*/
         System.out.println(myAge);
+
+
+        /* Reference types*/
+        Date now = new Date();
+        System.out.println(now);
+
     }
 }
