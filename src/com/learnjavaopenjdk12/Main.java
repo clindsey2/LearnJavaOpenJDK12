c71f5adbbcc9129e8a9c4cba728d29f07075550b
clindsey2 <clindsey2@liberty.edu>
2021-11-12 10:59:52 -0500

    primitive types

--- src/com/learnjavaopenjdk12/Main.java
+++ src/com/learnjavaopenjdk12/Main.java
@@ -3,8 +3,11 @@ package com.learnjavaopenjdk12;
 public class Main {
 
     public static void main(String[] args) {
-        int myAge = 30;
-        int herAge = myAge;
-        System.out.println(herAge);
+        byte myAge = 30;
+        long viewsCount = 3_123_456_789L;    /*underscores make the number more readable & L indicates long*/
+        float price = 10.99F;   /*the F tells java to store this as a float*/
+        char letter = 'A';  /* wrap single character with single quote and multiple characters with double quotes*/
+        boolean isEligible = false;   /*words in orange are reserved keywords*/
+        System.out.println(myAge);
     }
 }
