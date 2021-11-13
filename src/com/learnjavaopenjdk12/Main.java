bccdbbbf5017c9a5442d13bda5e9cead01076232
clindsey2 <clindsey2@liberty.edu>
2021-11-12 21:15:51 -0500

    reference types

--- src/com/learnjavaopenjdk12/Main.java
+++ src/com/learnjavaopenjdk12/Main.java
@@ -1,5 +1,6 @@
 package com.learnjavaopenjdk12;
 
+import java.awt.*;
 import java.util.Date;
 
 public class Main {
@@ -17,6 +18,18 @@ public class Main {
         /* Reference types*/
         Date now = new Date();
         System.out.println(now);
+
+        /* reference types */
+        byte x = 1;
+        byte y = x;
+        x = 2;
+        System.out.println(y);
+
+        Point point1 = new Point(1, 1);
+        Point point2 = point1; /* point2 points to the memory location of point1 */
+                               /* referencing to the same object */
+        point1.x = 2;
+        System.out.println(point2);
 
     }
 }
