a843f09045f6e5202eabdad1145ff934be53677c
clindsey2 <clindsey2@liberty.edu>
2021-11-13 09:52:34 -0500

    arithmetic expressions

--- src/com/learnjavaopenjdk12/Main.java
+++ src/com/learnjavaopenjdk12/Main.java
@@ -93,5 +93,40 @@ public class Main {
         System.out.println(Arrays.deepToString(threeDCurlyNumArray));
 
 
+        /* constants */
+        final float pi = 3.14F;  // represent as float with F -- final treats it as a constant
+
+        /* arithmetic expressions  */
+        int addResults = 10 + 3;
+        int subResults = 10 - 3;
+        int multResults = 10 * 3;
+        double divResults = (double)10 / (double)3;
+        int modResults = 10 % 3;
+        System.out.println(addResults);
+        System.out.println(subResults);
+        System.out.println(multResults);
+        System.out.println(divResults);
+        System.out.println(modResults);
+        int xae = 1;
+        xae++;
+        int yae = xae++;
+        int zae = ++xae;
+        System.out.println(xae);
+        System.out.println(yae);
+        System.out.println(zae);
+
+        int xaug = 4;
+        /* other augment operations: xaug -= 2; xaug *= 2; xaug /= 2; */
+        xaug += 2;
+        System.out.println(xaug);
+
+        /* order of operators */
+        int xops = 10 + 3 * 2;
+        int yops = (10 + 3) * 2;
+        System.out.println(xops);
+        System.out.println(yops);
+
+
+
     }
 }
