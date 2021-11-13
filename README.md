0f14733ea71b59619c441b1d6f4412adcefe7f69
clindsey2 <clindsey2@liberty.edu>
2021-11-13 10:50:34 -0500

    the Math class

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Working Notes from: Java Tutorial for Beginners
 https://www.youtube.com/watch?v=eIrMbAQSU34
 
---left off at 1:12:35 of 2:30:47
+--left off at 1:17:10 of 2:30:47
 
 
 Other Mosh training videos:
--- src/com/learnjavaopenjdk12/Main.java
+++ src/com/learnjavaopenjdk12/Main.java
@@ -151,5 +151,22 @@ public class Main {
         System.out.println(yfromI);
         System.out.println(zfromD);
 
+        /* the Math Class */
+        int resultI = Math.round(1.1F);
+        int resultF = (int)Math.floor(1.1F);
+        int resultC = (int)Math.ceil(1.1F);
+        System.out.printf("value=%d floor=%d and ceiling=%d\n",resultI, resultF, resultC);
+
+        int resultMax = Math.max(1, 2);
+        int resultMin = Math.min(12, 38);
+        double randomNum = Math.random(); // random number between zero and one
+        int randomIntLT100 = (int) Math.round(Math.random() * 100) ;
+
+        System.out.printf("the max of 1, 2 is = %d\n", resultMax);
+        System.out.printf("the min of 12, 38 is = %d\n", resultMin);
+        System.out.printf("the next random number is %s(string) or  %f(float)\n", randomNum, randomNum);
+        System.out.printf("the next random number less than 100 = %d", randomIntLT100);
+
+
     }
 }
