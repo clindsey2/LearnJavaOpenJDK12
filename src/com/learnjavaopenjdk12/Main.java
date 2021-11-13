f6811ae13dc7e507cb8bfa58bc6f6f78813f2c26
clindsey2 <clindsey2@liberty.edu>
2021-11-13 00:03:12 -0500

    arrays

--- src/com/learnjavaopenjdk12/Main.java
+++ src/com/learnjavaopenjdk12/Main.java
@@ -1,6 +1,7 @@
 package com.learnjavaopenjdk12;
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
@@ -63,6 +64,33 @@ public class Main {
         String backslashMessage = "C:\\Windows\\...\\t...\\n...";
         System.out.println(backslashMessage);
 
+        /* one dimensional arrays */
+        int number = 1;
+        int[] numberArray = new int[5];  /* array of integer */
+        numberArray[0] = 1;
+        numberArray[1] = 2;
+        // another means to load an array
+        int[] numbers = { 2, 7, 11, 5, 3, 9, 4};
+        Arrays.sort(numbers);
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(numberArray));
+
+        // multidimensional arrays
+        int [][] twoDNumArray = new int[2][3];
+        twoDNumArray[0][0] = 1;
+        // with curly braces
+        int[][] twoDCurlyNumArray = {{1, 2, 3}, {4, 5, 6} };
+        System.out.println(Arrays.deepToString(twoDNumArray));
+        System.out.println(Arrays.deepToString(twoDCurlyNumArray));
+
+
+        int[][][] threeDNumArray = new int[2][3][5];
+        // using curly braces
+        int[][][] threeDCurlyNumArray = { { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} },
+                {{22, 33, 222, 333 }, {44, 55, 444, 555}, {66, 77, 666, 777} } };
+        System.out.println(Arrays.deepToString(threeDNumArray));
+        System.out.println(Arrays.deepToString(threeDCurlyNumArray));
 
 
     }
