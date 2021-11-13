19f4edfa2741abc7e01f884ce26edd12094839fa
clindsey2 <clindsey2@liberty.edu>
2021-11-12 21:38:58 -0500

    strings

--- src/com/learnjavaopenjdk12/Main.java
+++ src/com/learnjavaopenjdk12/Main.java
@@ -1,7 +1,9 @@
 package com.learnjavaopenjdk12;
 
 import java.awt.*;
+import java.sql.SQLOutput;
 import java.util.Date;
+import java.util.Locale;
 
 public class Main {
 
@@ -30,6 +32,31 @@ public class Main {
                                /* referencing to the same object */
         point1.x = 2;
         System.out.println(point2);
+
+        /* Strings */
+        /* Since strings are a reference type, you would expect to use this to define the variable message
+        String message = new String("Hello World");
+          However, there is a shorter way to initialize string variables, see below  */
+        String message = "Hello World";
+        String longerMessage = "Hello World" + "!!";
+        longerMessage.endsWith("!!");
+        /* System.out.println(longerMessage); */
+        System.out.println(longerMessage.startsWith("!!"));
+        System.out.println(longerMessage.endsWith("!!"));
+        System.out.println(message.length());
+        System.out.println(longerMessage.length());
+        System.out.println(longerMessage.indexOf("sky"));
+        System.out.println(longerMessage.replace("!","*"));
+        /* However, this does not change the original string longerMessage */
+        /* since in Java Strings are immutable, we cannot change/mutate them */
+        System.out.println(longerMessage);
+        System.out.println(message.toLowerCase());
+        /* and again, the original String message is not changed */
+        System.out.println(message);
+        /* there is also a trim method to get rid of spaces at the beginning or ending of a string  */
+        String spaceyMessage = "   Hello Again       ";
+        System.out.println(spaceyMessage.trim());
+        System.out.println(spaceyMessage);
 
     }
 }
